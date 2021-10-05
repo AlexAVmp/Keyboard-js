9f6b1a5304c7df0502ae006f08a61a3995050f41
AlexAVmp <al.averyn.new@gmail.com>
2021-10-05 23:47:36 +0300

    feat: add some functional part

--- main.js
+++ main.js
@@ -1,38 +1,51 @@
-const Keyboard = {
-    elements: {
-        main: null,
-        container: null,
-        input_field: null
-    },
+import letters from "./buttons.js";
 
-    load(){
-        this.elements.main = document.createElement("div");
-        this.elements.container = document.createElement("div");
-        this.elements.input_field = document.createElement("textarea");
-        
-        this.elements.main.classList.add("keyboard");
-        this.elements.container.classList.add("keyboard__keys");
-        this.elements.input_field.classList.add('keyboard__input-field');
-
-        this.elements.container.appendChild(this.createKeys());
-        this.elements.main.appendChild(this.elements.container);
-        document.body.appendChild(this.elements.main);
-    },
-
-    createKeys(){
-        const fragment = document.createDocumentFragment();
-
-        let keys = [];
-        if (this.properties.language === 'eng') {
-            keys = this.languages.english;
-        } else if (this.properties.language === 'rus') {
-            keys = this.languages.rus;
-        }
-
-        const keyCodes = ['Backquote', 'Digit1', 'Digit2', 'Digit3', 'Digit4', 'Digit5', 'Digit6', 'Digit7', 'Digit8', 'Digit9', 'Digit0',
-        'Minus', 'Equal', 'Backspace', 'Tab', 'KeyQ', 'KeyW', 'KeyE', 'KeyR', 'KeyT', 'KeyY', 'KeyU', 'KeyI', 'KeyO', 'KeyP', 'BracketLeft',
-        'BracketRight', 'Backslash', 'Delete', 'CapsLock', 'KeyA', 'KeyS', 'KeyD', 'KeyF', 'KeyG', 'KeyH', 'KeyJ', 'KeyK', 'KeyL', 'Semicolon',
-        'Quote', 'Enter', 'ShiftLeft', 'KeyZ', 'KeyX', 'KeyC', 'KeyV', 'KeyB', 'KeyN', 'KeyM', 'Comma', 'Period', 'Slash', 'ShiftRight', 'ArrowUp',
-        'ControlLeft', 'MetaLeft', 'AltLeft', 'Space', 'AltRight', 'ControlRight', 'ArrowLeft', 'ArrowDown', 'ArrowRight'];
-    }
+const languages = {
+    ru: "ru",
+    en: "en",
 }
+
+const lang = languages.ru;
+
+const main = document.createElement('main');
+document.body.appendChild(main);
+main.classList.add('main');
+
+const textArea = document.createElement('textarea');
+document.body.appendChild(textArea);
+textArea.classList.add('text-area')
+textArea.setAttribute('cols', 100);
+
+const keyboard = document.createElement('div');
+main.appendChild(keyboard);
+main.classList.add('keyboard');
+
+let line = document.createElement('div');
+
+letters.forEach((element) => {
+    const button = document.createElement('button');
+    button.classList.add(element.code);
+    button.classList.add(element.type);
+    button.classList.add(element.width);
+    button.innerHTML = element.text[lang];
+
+    const lastButtonLine = element.code;
+    if (lastButtonLine === 'Backquote' || lastButtonLine === 'Tab' || lastButtonLine === 'CapsLock' || lastButtonLine === 'ShiftLeft' || lastButtonLine === 'ControlLeft') {
+        line = document.createElement('div');
+        keyboard.appendChild(line);
+        line.classList.add('line')
+    }
+    line.appendChild(button);
+})
+
+textArea.addEventListener('blur', () => {
+    textArea.focus();
+});
+textArea.focus();
+
+const capsLock = document.querySelector('.CapsLock');
+const caseButton = document.querySelectorAll('button');
+
+textArea.addEventListener('keyup', (el) => {
+    
+})
